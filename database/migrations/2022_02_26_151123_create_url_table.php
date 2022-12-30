98c1d3fb5faa0a9081b29412c37250f0f7601e00
thiiagoms <thiagom.devsec@gmail.com>
2022-12-29 21:16:22 -0300

    feat: change url migration fields :fire:

--- database/migrations/2022_02_26_151123_create_url_table.php
+++ database/migrations/2022_02_26_151123_create_url_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
-     */
+    */
     public function up()
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->string('original');
-            $table->string('short');
-            $table->integer('clicks')->default(0);
+            $table->string('origin')->nullable(false);
+            $table->string('short')->nullable(false);
+            $table->integer('clicks')->default(0)->nullable(false);
             $table->timestamps();
         });
     }
